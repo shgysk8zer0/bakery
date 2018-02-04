d519f9cc71300b75285fc3d37fbc681cda422a13
Chris Zuber <shgysk8zer0@gmail.com>
2018-02-04 01:48:21 -0800

    Enhance gallery with next/prev and timed cycling of images

--- js/gallery.js
+++ js/gallery.js
@@ -1,21 +1,70 @@
-import {$, ready} from './std-js/functions.js';
+import {$, ready, imgur, wait} from './std-js/functions.js';
 
-ready().then(() => {
-	$('.gallery').each(gallery => {
-		$('.thumbnails picture:first-of-type').addClass('current-thumb');
-		$('.thumbnails picture', gallery).click(event => {
-			const clicked = event.target.closest('.thumbnails picture');
-			const current = gallery.querySelector('.current').parentElement;
-			console.log({clicked, current});
-			const thumbnails = clicked.closest('.thumbnails');
-			$('picture', thumbnails).forEach(thumbnail => {
-				thumbnail.classList.toggle('current-thumb', thumbnail === clicked);
-			});
-			const clone = clicked.cloneNode(true);
-			const sizes = current.querySelector('source').sizes;
-			$('source', clone).forEach(source => source.sizes = sizes);
-			clone.querySelector('img').className = current.querySelector('img').className;
-			current.replaceWith(clone);
+ready().then(async () => {
+	function* getThumbs(gallery) {
+		while(true) {
+			for (const thumb of $('.thumbnails [data-imgur]', gallery)) {
+				yield thumb;
+			}
+		}
+	}
+
+	async function setGalleryImage(thumb, gallery) {
+		gallery.classList.add('cursor-wait', 'no-pointer-events');
+		const thumbnails = thumb.closest('.thumbnails');
+		const current = gallery.querySelector('.current');
+		$('picture', thumbnails).toggleClass('current-thumb', thumbnail => thumbnail === thumb);
+
+		gallery.querySelector('figcaption').textContent = thumb.dataset.caption;
+		const pic = await imgur(thumb.dataset.imgur, {
+			sizes: ['(min-width: 700px) 75vw', '100vw'],
 		});
+		pic.dataset.imgur = thumb.dataset.imgur;
+		pic.classList.add('current', 'animation-fill-both', 'animation-speed-normal', 'animation-ease-in-out', 'fadeIn');
+		gallery.classList.remove('cursor-wait', 'no-pointer-events');
+
+		current.classList.add('animation-fill-both', 'animation-speed-normal', 'animation-ease-in-out');
+		current.classList.remove('fadeIn');
+		current.classList.add('fadeOut');
+		await wait(400);
+		current.replaceWith(pic);
+	}
+
+	$('.gallery').each(async gallery => {
+		const thumbs = getThumbs(gallery);
+		let thumb = thumbs.next().value;
+
+		$('button.next', gallery).click(() => {
+			thumb = thumbs.next().value;
+			setGalleryImage(thumb, gallery);
+		});
+
+		$('button.prev', gallery).click(() => {
+			const prev = thumb.previousElementSibling || thumb.parentElement.lastElementChild;
+			if (thumb instanceof Element && prev instanceof Element) {
+				while (prev !== thumb) {
+					thumb = thumbs.next().value;
+				}
+				setGalleryImage(thumb, gallery);
+			}
+		});
+
+		$('.thumbnails picture:first-of-type').addClass('current-thumb');
+		$('.thumbnails [data-imgur] img', gallery).click(event => {
+			const clicked = event.target.closest('.thumbnails [data-imgur]');
+			setGalleryImage(clicked, gallery);
+			while(thumb !== clicked) {
+				thumb = thumbs.next().value;
+			}
+		});
+
+		/*eslint no-constant-condition: "off" */
+		while(true) {
+			await wait(6000);
+			if (document.visibilityState !== 'hidden') {
+				thumb = thumbs.next().value;
+				await setGalleryImage(thumb, gallery);
+			}
+		}
 	});
 });
